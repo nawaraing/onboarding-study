e2507bd2b317a1247bc470c90516a064ff3ffa7b
JunKang <nawaddaing@gmail.com>
2024-08-23 15:39:42 +0900

    [UPDATE] SheetJS backend

--- spring-mvc/spring_example/src/main/java/com/example/spring_example/sheetjs/UploadPageController.java
+++ spring-mvc/spring_example/src/main/java/com/example/spring_example/sheetjs/UploadPageController.java
@@ -2,16 +2,37 @@ package com.example.spring_example.sheetjs;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.spring_example.sheetjs.model.ServiceLink;
+import java.util.List;
 
 @Controller
 public class UploadPageController {
     
-    private final String REQUEST_URL = "/upload_page";
+    private final String UPLOAD_PAGE_URL = "/upload-page";
+    private final String SEND_DATA_URL = "/send-data";
 
-    @GetMapping(REQUEST_URL)
+    @GetMapping(UPLOAD_PAGE_URL)
     public String uploadPage() {
         System.out.println("uploadPage");
         return "/sheetjs/uploadSheetjs";
+    }
+
+    @PostMapping(SEND_DATA_URL)
+    public void sendData(@RequestBody List<ServiceLink> serviceLinkList) {
+        System.out.println("sendData");
+        for (ServiceLink serviceLink : serviceLinkList) {
+            System.out.println("=============================");
+            System.out.println("helperNm: " + serviceLink.getHelperNm());
+            System.out.println("helperBirthday: " + serviceLink.getHelperBirthday());
+            System.out.println("helperGenderNm: " + serviceLink.getHelperGenderNm());
+            System.out.println("clientNm: " + serviceLink.getClientNm());
+            System.out.println("clientBirthday: " + serviceLink.getClientBirthday());
+            System.out.println("clientGenderNm: " + serviceLink.getClientGenderNm());
+            System.out.println("svcStartDt: " + serviceLink.getSvcStartDt());
+            System.out.println("svcEndDt: " + serviceLink.getSvcEndDt());
+        }
     }
 }
---
+++ spring-mvc/spring_example/src/main/java/com/example/spring_example/sheetjs/model/ServiceLink.java
@@ -0,0 +1,62 @@
+package com.example.spring_example.sheetjs.model;
+
+public class ServiceLink {
+    private String helperNm;
+    private String helperBirthday;
+    private String helperGenderNm;
+    private String clientNm;
+    private String clientBirthday;
+    private String clientGenderNm;
+    private String svcStartDt;
+    private String svcEndDt;
+
+    public void setHelperNm(String helperNm) {
+        this.helperNm = helperNm;
+    }
+    public String getHelperNm() {
+        return helperNm;
+    }
+    public void setHelperBirthday(String helperBirthday) {
+        this.helperBirthday = helperBirthday;
+    }
+    public String getHelperBirthday() {
+        return helperBirthday;
+    }
+    public void setHelperGenderNm(String helperGenderNm) {
+        this.helperGenderNm = helperGenderNm;
+    }
+    public String getHelperGenderNm() {
+        return helperGenderNm;
+    }
+    public void setClientNm(String clientNm) {
+        this.clientNm = clientNm;
+    }
+    public String getClientNm() {
+        return clientNm;
+    }
+    public void setClientBirthday(String clientBirthday) {
+        this.clientBirthday = clientBirthday;
+    }
+    public String getClientBirthday() {
+        return clientBirthday;
+    }
+    public void setClientGenderNm(String clientGenderNm) {
+        this.clientGenderNm = clientGenderNm;
+    }
+    public String getClientGenderNm() {
+        return clientGenderNm;
+    }
+    public void setSvcStartDt(String svcStartDt) {
+        this.svcStartDt = svcStartDt;
+    }
+    public String getSvcStartDt() {
+        return svcStartDt;
+    }
+    public void setSvcEndDt(String svcEndDt) {
+        this.svcEndDt = svcEndDt;
+    }
+    public String getSvcEndDt() {
+        return svcEndDt;
+    }
+
+}
